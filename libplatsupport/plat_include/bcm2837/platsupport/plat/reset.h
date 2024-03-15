8da25f6cfff261a94ac467b5577453d875c016f6
Sebastian Eckl <sebastian.eckl@hensoldt.net>
2024-03-15 18:45:20 +0100

    [DEV] provide dummy reset.h files for all platforms to adapt to changes induced by Nvidia platform support

---
+++ libplatsupport/plat_include/bcm2837/platsupport/plat/reset.h
@@ -0,0 +1,10 @@
+/*
+ * Dummy reset.h (to be aligned with mach_include/bcm in the future)
+ *
+ * Copyright (C) 2022, HENSOLDT Cyber GmbH
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+enum reset_id {
+    NRESETS
+};
---
+++ libplatsupport/plat_include/qemu-arm-virt/platsupport/plat/reset.h
@@ -0,0 +1,10 @@
+/*
+ * Dummy reset.h
+ *
+ * Copyright (C) 2022, HENSOLDT Cyber GmbH
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+enum reset_id {
+    NRESETS
+};
